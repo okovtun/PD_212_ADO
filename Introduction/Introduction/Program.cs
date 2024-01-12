65f132da00dd5ab192649dc48087dbbf002f337e
Clayman <Clayman@DESKTOP-DM8E77U>
2024-01-12 17:27:38 +0300

    Introduction to ADO

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Introduction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LibraryPD_212;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			SqlConnection connection = new SqlConnection(connection_string);    //Создаем соединение с сервером. При создании оно не активно.
+			connection.Open();  //Открываем соединение, поскольку автоматически оно не открывается.
+
+			//Вставка:
+			//string insert_string = @"INSERT INTO Authors (first_name, last_name) VALUES ('Steven', 'Hawking')";
+			string insert_string = @"INSERT INTO Books (author, title, price, pages) VALUES (4, 'The Steven Hawkings Universe', 160, 850)";
+			SqlCommand cmd = new SqlCommand(insert_string, connection);
+			//cmd.ExecuteNonQuery();  //INSERT, UPDATE, DELETE
+
+			//Выборка:
+			string select_string = @"SELECT * FROM Authors";
+			cmd.CommandText = select_string;
+
+			SqlDataReader rdr = cmd.ExecuteReader();
+			while (rdr.Read())
+			{
+				Console.WriteLine($"{rdr[0]} {rdr[1]} {rdr[2]}");
+			}
+
+			connection.Close();	//Соединения обязательно нужно закрывать
+		}
+	}
+}
---
+++ Introduction/Introduction/Queries/Query1 - CREATE Authors & Books.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Authors]
+(
+	[id]			INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[first_name]	NVARCHAR(100)	NOT NULL,
+	[last_name]		NVARCHAR(100)	NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[Books]
+(
+	[id]			INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[author]		INT NOT NULL,
+	FOREIGN KEY (author)	REFERENCES Authors (id),
+	[title]			NVARCHAR(200) NOT NULL,
+	[price]			MONEY,
+	[pages]			INT
+)
+GO
